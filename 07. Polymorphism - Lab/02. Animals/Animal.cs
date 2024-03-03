1e4a928ea369249b28a7f019c488302f8e33d810
Ber0v <AlexBerov123@gmail.com>
2024-03-03 16:44:51 +0200

    Polymorphism - Lab
    solving all tasks from Polymorphism - Lab

---
+++ 07. Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,17 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+            => $"I am {this.Name} and my favorite food is {this.FavouriteFood}";
+    }
+}
