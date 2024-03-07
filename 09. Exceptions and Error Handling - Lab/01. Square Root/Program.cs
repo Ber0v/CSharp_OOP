f086a987e3ea14b1bbdeb603b0be5570f1179c27
Ber0v <AlexBerov123@gmail.com>
2024-03-07 17:46:47 +0200

    Exceptions and Error Handling - Lab
    solving all tasks from Exceptions and Error Handling - Lab

---
+++ 09. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+    }
+}
---
+++ 09. Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Enter_Numbers
+{
+    class Program
+    {
+        public static int start = 1;
+        public static int end = 100;
+        public static List<int> numbers = new List<int>();
+        static void Main(string[] args)
+        {
+            ReadNumber();
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static void ReadNumber()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                try
+                {
+                    string number = Console.ReadLine();
+                    int n = 0;
+                    if (!int.TryParse(number, out n))
+                    {
+                        throw new ArgumentException("Invalid Number!");
+                    }
+                    else if (int.Parse(number) <= start || int.Parse(number) >= end)
+                    {
+                        throw new ArgumentException($"Your number is not in range {start} - 100!");
+                    }
+                    else
+                    {
+                        start = int.Parse(number);
+                        numbers.Add(int.Parse(number));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    i--;
+                }
+            }
+        }
+    }
+}
